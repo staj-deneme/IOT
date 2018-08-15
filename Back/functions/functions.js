623bbb8d765e0be7893ef83214ec76cef1e2c337
eminuzn <eminzun@gmail.com>
2018-08-15 17:06:40 +0300

    Htrms fonksiyonu tamam

--- Back/functions/functions.js
+++ Back/functions/functions.js
@@ -1,5 +1,3 @@
-
-
 function Øopr(Øs,Øair){
     return 0.7*Øs+0.3*Øair
 }
@@ -7,4 +5,16 @@ function Øopr(Øs,Øair){
 function Øs(Øair,Htris,Hve,Øsup,Φia){
     return Øair*(Htris+Hve)-(Hve-Øsup)-Htris-Φia
 }
+// Hve 26. sayfada
 
+
+/**
+ *  İç yüzey ve kütle arasında ısı geçiş katsayısı
+ *  Htr,ms = hms . Am
+ *  hms = 9,1 W/m²K sabit 
+ *  Am  = Tablo 1.1’ deki 3 değer zone kaydı yapılırken alınarak Am için kullanılır. 
+ *  
+ */
+ function Htrms(Am) {
+     return (9.1*Am);
+ }
