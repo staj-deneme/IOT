edf6c023e022385428106240a6f991d81436f744
ezgigozen <42175531+ezgigozen@users.noreply.github.com>
2018-08-16 17:11:50 +0300

    ezgi

--- Back/functions/functions.js
+++ Back/functions/functions.js
@@ -1,10 +1,87 @@
-
-
-function Øopr(Øs,Øair){
-    return 0.7*Øs+0.3*Øair
+function Øopr(Øs, Øair) {
+    return 0.7 * Øs + 0.3 * Øair
 }
 //Øs=Øair(Htris+Hve)-(Hve-Øsup)-Htris-Φia
-function Øs(Øair,Htris,Hve,Øsup,Φia){
-    return Øair*(Htris+Hve)-(Hve-Øsup)-Htris-Φia
+function Øs(Øair, Htris, Hve, Øsup, Φia) {
+    return Øair * (Htris + Hve) - (Hve - Øsup) - Htris - Φia
+
 }
 
+
+/*Htris: İç yüzey ile iç ortam arasındakı ısı geçis katsayısı -syf:38
+Htris = his*Atot
+his : 3.45 (sabit katsayı)
+Atot : bir zonu çevreleyen tüm iç yüzeylerin alanları toplamı 
+*/
+function Htris(Atot) {
+    const his = 3.45
+    return Atot * his
+}
+
+/* Htrop: Zonu çevreleyen opak yüzeylerin iletim ve taşınım ısı geçiş katsayısı -syf:10
+  Aopi: Opak yüzeyin alanı, m^2
+  Uopi: Opak bileşenin ısıl geçirgenlik katsayısı, W/(m²*K)
+  Lopi: Doğrusal ısı köprüsü uzunluğu, m
+  Ψoi: Doğrusal ısı köprüsü birim uzunluk başına ısı geçiş katsayısı, W/(m*K)
+
+ Htrop= Σ Aopi*Uopi + Σ Lopi*Ψoi
+
+  kod: ısı köprüsü tipi (tablo 9.1)
+  opbil: opak bileşen dizisi
+*/
+function Htrop(kod) {
+    var Uopi = Uopi();
+    var sum= 0
+ opbil.forEach(element =>{
+     sum += ((element.Aopi)*Uopi) + (element*Lopi)*(tablolar.tablo9_1[kod.Ψoi])
+ });
+return sum
+}
+
+/*  Uopi: Opak bir bileşenin ısıl geçirgenlik katsayısı - syf:10
+hsi: İç yüzey ısıl taşınım katsayısı, W/(m^2.K)
+hse: Dış yüzey ısı taşınım katsayısı, W/(m^2.K)
+dl: l nci malzemenin kalınlığı, m 
+λhl: l nci malzemenin ısıl iletkenlik hesap değeri, W/(m.K)
+Rgap: Opak bileşen malzemeleri arasındaki boşluğun ısıl direnci, m²K/W
+
+Uopi = 1/(1/hsi + Σ dl/λhl + 1/hse +Rgap)
+
+tip: yapı bileşen tipi (tablo 3.1)
+yon: yatay/dusey (tablo 3.2)
+bosluk_kal: boşluk kalınlığı (tablo 3.2)
+malzeme : opak bileşenin oluştuğu malzeme dizisi
+*/
+
+function Uopi(tip,yon,bosluk_kal) {
+    var hsi = 1 / (tablolar.tablo3_1[tip.Rsi])
+    var hse = 1 / (tablolar.tablo3_1[tip.Rse])
+    var sum = 0
+    opbil.malzeme.forEach(element => {
+        sum += (element.dl / element.λhl)
+    });
+    tablolar.tablo3_2[yon].forEach(element => {
+        if(element.min <= bosluk_kal && element.max >=bosluk_kal){
+         Rgap = element.Rgap
+        }
+
+    });
+    return 1 / ((1 / hsi) + sum + (1 / hse) + Rgap)
+}
+
+/* Φia : İç hava sıcaklığına etki eden ısı kazanç miktarı 
+*/
+function Φia (){
+    var Φint = ofisΦint(); 
+    return 0.5* Φint
+}
+
+/* Ofis binalarında iç kazanç
+ */
+function ofisΦint (Np,Af,Φintlg){
+ var Φintoc = 130*Np
+ var ΦintAppunit
+ var ΦintApp = Af*
+
+
+}
---
+++ Back/functions/tables.js
@@ -0,0 +1,79 @@
+var tablolar = {
+    tablo3_1: {
+        tip1: { Rsi: 0.13, Rse: 0.04 },
+        tip2: { Rsi: 0.13, Rse: 0.08 },
+        tip3: { Rsi: 0.13, Rse: 0 },
+        tip4: { Rsi: 0.17, Rse: 0 },
+        tip5: { Rsi: 0.17, Rse: 0.17 },
+        tip6: { Rsi: 0.17, Rse: 0 },
+        tip7: { Rsi: 0.13, Rse: 0.08 },
+        tip8: { Rsi: 0.17, Rse: 0.04 },
+        tip9: { Rsi: 0.13, Rse: 0.13 },
+        tip10: { Rsi: 0.13, Rse: 0.13 },
+        tip11: { Rsi: 0.13, Rse: 0.04 }
+    },
+    tablo3_2 = {
+        dusey: [
+            { min: 0.1, max: 10 ,Rgap:0.14},
+            { min: 11, max: 20, Rgap:0.16},
+            { min: 21, max: 50, Rgap:0.18},
+            { min: 51, max: 100, Rgap: 0.17},
+            { min: 100, max: 999999999999999999, Rgap: 0.16}
+        ],
+        yatay: [
+            {min: 0.1, max:10, Rgap: 0.14},
+            {min: 11, max:20, Rgap: 0.15},
+            {min: 20, max:9999999999999999, Rgap:0.16}
+        ]
+    },
+    
+    tablo9_1: {
+        C1: { Ad: "Çatı", Ψe: 0.55, Ψoi: 0.75, Ψi: 0.75 },
+        C2: { Ad: "Çatı", Ψe: 0.5, Ψoi: 0.75, Ψi: 0.75 },
+        C3: { Ad: "Çatı", Ψe: 0.4, Ψoi: 0.75, Ψi: 0.75 },
+        C4: { Ad: "Çatı", Ψe: 0.4, Ψoi: 0.65, Ψi: 0.65 },
+        C5: { Ad: "Çatı", Ψe: 0.6, Ψoi: 0.8, Ψi: 0.8 },
+        C6: { Ad: "Çatı", Ψe: 0.5, Ψoi: 0.7, Ψi: 0.7 },
+        C7: { Ad: "Çatı", Ψe: 0.65, Ψoi: 0.85, Ψi: 0.85 },
+        C8: { Ad: "Çatı", Ψe: 0.45, Ψoi: 0.7, Ψi: 0.7 },
+        C9: { Ad: "Çatı", Ψe: -0.05, Ψoi: 0.15, Ψi: 0.15 },
+        C10: { Ad: "Çatı", Ψe: 0, Ψoi: 0.2, Ψi: 0.2 },
+        C11: { Ad: "Çatı", Ψe: 0.05, Ψoi: 0.25, Ψi: 0.25 },
+        C12: { Ad: "Çatı", Ψe: 0.15, Ψoi: 0.4, Ψi: 0.4 },
+        B1: { Ad: "Balkon", Ψe: 0.95, Ψoi: 0.95, Ψi: 1.05 },
+        B2: { Ad: "Balkon", Ψe: 0.95, Ψoi: 0.95, Ψi: 1.05 },
+        B3: { Ad: "Balkon", Ψe: 0.9, Ψoi: 0.9, Ψi: 1 },
+        B4: { Ad: "Balkon", Ψe: 0.7, Ψoi: 0.7, Ψi: 0.8 },
+        AK1: { Ad: "Asma Kat", Ψe: 0, Ψoi: 0, Ψi: 0.1 },
+        AK2: { Ad: "Asma Kat", Ψe: 0.95, Ψoi: 1.05, Ψi: 1.05 },
+        AK3: { Ad: "Asma Kat", Ψe: 0.9, Ψoi: 1, Ψi: 1 },
+        AK4: { Ad: "Asma Kat", Ψe: 0.7, Ψoi: 0.8, Ψi: 0.8 },
+        AK5: { Ad: "Asma Kat", Ψe: 0.6, Ψoi: 0.65, Ψi: 0.65 },
+        AK6: { Ad: "Asma Kat", Ψe: 0.9, Ψoi: 1, Ψi: 1 },
+        AK7: { Ad: "Asma Kat", Ψe: 0.7, Ψoi: 0.8, Ψi: 0.8 },
+        AK8: { Ad: "Asma Kat", Ψe: 0.45, Ψoi: 0.6, Ψi: 0.6 },
+        TUD1: { Ad: "Toprak Üstü Yüzer Döşeme", Ψe: 0.65, Ψoi: 0.8, Ψi: 0.8 },
+        TUD2: { Ad: "Toprak Üstü Yüzer Döşeme", Ψe: 0.6, Ψoi: 0.75, Ψi: 0.75 },
+        TUD3: { Ad: "Toprak Üstü Yüzer Döşeme", Ψe: 0.55, Ψoi: 0.7, Ψi: 0.7 },
+        TUD4: { Ad: "Toprak Üstü Yüzer Döşeme", Ψe: 0.5, Ψoi: 0.65, Ψi: 0.65 },
+        TUD5: { Ad: "Toprak Üstü Yüzer Döşeme", Ψe: 0.6, Ψoi: 0.75, Ψi: 0.75 },
+        TUD6: { Ad: "Toprak Üstü Yüzer Döşeme", Ψe: 0.45, Ψoi: 0.6, Ψi: 0.6 },
+        TUD7: { Ad: "Toprak Üstü Yüzer Döşeme", Ψe: -0.05, Ψoi: 0.1, Ψi: 0.1 },
+        TUD8: { Ad: "Toprak Üstü Yüzer Döşeme", Ψe: 0.05, Ψoi: 0.2, Ψi: 0.2 },
+        AGK1: { Ad: "Asma Giriş Kat", Ψe: 0.75, Ψoi: 0.95, Ψi: 0.95 },
+        AGK2: { Ad: "Asma Giriş Kat", Ψe: 0.65, Ψoi: 0.85, Ψi: 0.85 },
+        AGK3: { Ad: "Asma Giriş Kat", Ψe: 0.55, Ψoi: 0.75, Ψi: 0.75 },
+        AGK4: { Ad: "Asma Giriş Kat", Ψe: 0.5, Ψoi: 0.7, Ψi: 0.7 },
+        AGK5: { Ad: "Asma Giriş Kat", Ψe: 0.6, Ψoi: 0.8, Ψi: 0.8 },
+        AGK6: { Ad: "Asma Giriş Kat", Ψe: 0.45, Ψoi: 0.65, Ψi: 0.65 },
+        AGK7: { Ad: "Asma Giriş Kat", Ψe: -0.1, Ψoi: 0.1, Ψi: 0.1 },
+        AGK8: { Ad: "Asma Giriş Kat", Ψe: 0, Ψoi: 0.2, Ψi: 0.2 },
+        KL1: { Ad: "Kolon", Ψe: 1.3, Ψoi: 1.3, Ψi: 1.3 },
+        KL2: { Ad: "Kolon", Ψe: 1.2, Ψoi: 1.2, Ψi: 1.2 },
+        KL3: { Ad: "Kolon", Ψe: 1.15, Ψoi: 1.15, Ψi: 1.15 },
+        KL4: { Ad: "Kolon", Ψe: 0.9, Ψoi: 0.9, Ψi: 0.9 },
+
+    }
+}
+
+
